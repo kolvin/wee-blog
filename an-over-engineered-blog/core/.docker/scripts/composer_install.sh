7d6a8d365e0b552c355e8ce12c209d4218ef5f09
Calvin Taylor <taylorcalvin07@gmail.com>
2021-04-28 11:24:35 +0100

    chore: site cleanup

--- an-over-engineered-blog/core/.docker/scripts/composer_install.sh
+++
@@ -1,1 +0,0 @@
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
--- an-over-engineered-blog/core/.docker/scripts/docker-purge.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Delete all containers
-docker rm -fv $(docker ps -a -q)
-# Delete all images
-docker rmi $(docker images -q)
-
-docker system prune
